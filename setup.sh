b059890fb1f86094c8bf0d5a19ae5f3052be03ac
Yuta Onodera <75541628+YutaOnodera@users.noreply.github.com>
2022-01-26 00:32:10 +0900

    Minor revisions

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-	# Install list
-	sudo apt install tmux
-	sudo apt-get install git-all
-	sudo apt install htop
+# Install list
+sudo apt install tmux
+sudo apt-get install git-all
+sudo apt install htop
